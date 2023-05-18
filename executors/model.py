b1aa32a99b27d2b3ec2195e1a85c8968e79dcc45
shubham-kanodia <shubhamkanodia123@gmail.com>
2023-05-18 13:04:04 +0530

    Change executor model

--- executors/model.py
+++ executors/model.py
@@ -2,7 +2,7 @@ from responses.response import Response
 
 
 class ModelExecutor:
-    def __init__(self, openai, system, model_name="gpt-4"):
+    def __init__(self, openai, system, model_name="gpt-4-32k-0314"):
         self.model_name = model_name
         self.system = system
         self.openai = openai
@@ -13,6 +13,21 @@ class ModelExecutor:
 
         resp = self.openai.ChatCompletion.create(
             model="gpt-4",
+            messages=self.messages
+        )
+
+        wrapped_response = Response(resp)
+        response_content = wrapped_response.content()
+
+        self.messages.append({"role": "assistant", "content": response_content})
+        return wrapped_response
+
+    def execute_all(self, messages):
+        for message in messages:
+            self.messages.append({"role": "user", "content": message})
+
+        resp = self.openai.ChatCompletion.create(
+            model=self.model_name,
             messages=self.messages
         )
 
